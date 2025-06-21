b9ea8d88c86247a66e43aa9baf28e790b5acd2db
ravimani1001 <inforavimani@gmail.com>
2025-06-21 22:42:13 +0530

    modified cookie properties

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // only send over HTTPS in prod
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -51,7 +51,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // only send over HTTPS in prod
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
